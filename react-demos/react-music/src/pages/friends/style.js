d89482e7f8a0b190a36ba642f543a6fe052db45c
wangwei <wangwei35422@163.com>
2021-07-23 21:38:26 +0800

    [react-music] feat: 完善朋友和我的音乐页面

---
+++ react-demos/react-music/src/pages/friends/style.js
@@ -0,0 +1,27 @@
+import styled from 'styled-components'
+
+import friendBg from '@/assets/img/friends.jpg'
+
+export const FriendWrapper = styled.div`
+  .content {
+    background-color: #fff;
+    min-height: 700px;
+
+    .pic {
+      position: relative;
+      width: 807px;
+      height: 484px;
+      margin: 0 auto;
+      background: url(${friendBg}) 0 104px no-repeat;
+
+      .login {
+        position: absolute;
+        width: 167px;
+        height: 45px;
+        left: 482px;
+        top: 368px;
+        text-indent: -9999px;
+      }
+    }
+  }
+`
