182eb8150e4e88157b9f176ee037a8e7bde337c9
wangwei <wangwei35422@163.com>
2021-09-18 15:46:18 +0800

    [koa2-demo/file-upload] feat: base64和像素点互转 生成图片

--- koa2-demo/file-upload/app.js
+++ koa2-demo/file-upload/app.js
@@ -1,5 +1,7 @@
 // 教程：https://www.huaweicloud.com/articles/2687012349fabd74e9cc9da11c0ca640.html
 const path = require('path');
+const { writeFileSync } = require('fs');
+const { Buffer } = require('buffer');
 const Koa = require('koa');
 // https://github.com/koajs/router/blob/master/API.md
 const Router = require('@koa/router');
@@ -9,6 +11,9 @@ const koaBody = require('koa-body');
 const koaStatic = require('koa-static');
 // https://github.com/koajs/cors
 // const cors = require('@koa/cors');
+const Crypto = require('crypto');
+const SizeOf = require('image-size');
+const images = require('images');
 
 const app = new Koa();
 const router = new Router();
@@ -18,9 +23,13 @@ const router = new Router();
 //   'Access-Control-Allow-Origin': '*'
 // }))
 
+function _getHash(salt = Date.now().toString()) {
+  return Crypto.createHash('md5').update(salt).digest('hex');
+}
+
 app.use(koaStatic(path.join(__dirname, 'public')));
 
-router.post('/upload', 
+router.post('/img', 
   koaBody({
     multipart: true, 
     // https://github.com/node-formidable/formidable
@@ -36,6 +45,61 @@ router.post('/upload',
     ctx.body = { 
       url: `${ctx.origin}/uploads/${basename}`,
       hash: file.hash
+    }
+  }
+);
+
+router.post('/pixel', 
+  koaBody(),
+  (ctx, next) => {
+    const data = ctx.request.body
+    // => POST body
+    const pixel = data.pixel
+    if (pixel && Array.isArray(pixel)) {
+      const sizeOld = pixel.length / 4
+      console.log(sizeOld)
+      const base64 = pixel.reduce((base, item) => {
+        const reg = /rgba\(([\d]+), ([\d]+), ([\d]+), 255\)/g;
+        const match = reg.exec(item).slice(1);
+
+        match.forEach(item => {
+          if (item !== 0) {
+            base += String.fromCharCode(item)
+          }
+        })
+        return base
+      }, '');
+
+      // 转图片
+      const buf = Buffer.from(base64, 'base64');
+      const unit = new Uint8Array(buf); // 返回一个被 string,编码格式是base64(默认编码格式是utf-8)的值初始化的新的 Buffer 实例
+      const salt = Date.now().toString();
+      const hash = _getHash(salt);
+      const imgPath = `${hash}.jpg`;
+
+      // 计算图片大小
+      const { width, height } = SizeOf(buf);
+      console.log(width, height)
+      const sizeNew = width * height;
+      const multiple = Math.abs((sizeOld / sizeNew));
+      const newWidth = multiple * width;
+      const newHeight = multiple * height;
+      const newHash = _getHash(salt + 'new');
+      console.log(multiple, newHeight, newWidth)
+
+      try {
+        // 用fs写入文件
+        const uploadDir = path.resolve(__dirname, `./public/uploads`);
+        writeFileSync(`${uploadDir}/${imgPath}`, unit);
+        images(buf).size(newWidth, newHeight).save(`${uploadDir}/${newHash}.jpg`);
+
+        ctx.body = {
+          url: `${ctx.origin}/uploads/${imgPath}`,
+          hash
+        };
+      } catch(err) {
+        console.log(err);
+      }
     }
   }
 );
