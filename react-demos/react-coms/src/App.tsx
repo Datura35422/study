2884e78582e3b3e5799d98d38096b3b42885a23f
wangwei <wangwei35422@163.com>
2021-11-06 00:20:07 +0800

    [react-coms] feat: init menu 组件

--- react-demos/react-coms/src/App.tsx
+++ react-demos/react-coms/src/App.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import Menu from './components/Menu/menu'
+import MenuItem from './components/Menu/menuItem'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h2> BUTTON </h2>
+        <Button>Default</Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Large}>Dangerous</Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Small}>Primary</Button>
+        <Button btnType={ButtonType.Link} href="www.baidu.com">Link</Button>
+      </div>
+      <hr />
+      <div>
+        <Menu onSelect={(index) => { alert(index) }}>
+          <MenuItem index={1}>menu 1</MenuItem>
+          <MenuItem index={2}>menu 2</MenuItem>
+          <MenuItem index={3}>menu 3</MenuItem>
+        </Menu>
+      </div>
     </div>
   );
 }
---
+++ react-demos/react-coms/src/components/Menu/menu.tsx
@@ -0,0 +1,54 @@
+import React, { useState, createContext } from 'react'
+import classNames from 'classnames'
+
+type MenuMode = 'horizontal' | 'vertical'
+type SelectCallback = (selectedIndex: number) => void
+
+export interface MenuProps {
+  defaultIndex?: number;
+  className?: string;
+  mode?: MenuMode;
+  style?: React.CSSProperties;
+  onSelect?: SelectCallback;
+}
+
+interface IMenuContext {
+  index: number;
+  onSelect?: SelectCallback;
+}
+
+const MenuContext = createContext<IMenuContext>({index: 0})
+
+const Menu: React.FC<MenuProps> = ({
+  className, mode, style, children, defaultIndex, onSelect
+}) => {
+  const [currentActive, setActive] = useState(defaultIndex || 0)
+  const classes = classNames('viking-menu', className, {
+    'menu-vertical': mode === 'vertical'
+  })
+  const handleClick = (index: number) => {
+    setActive(index)
+    onSelect && onSelect(index)
+  }
+  const passedContext: IMenuContext = {
+    index: currentActive,
+    onSelect: handleClick
+  }
+  return (
+    <ul className={classes} style={style}>
+      <MenuContext.Provider value={passedContext}>
+        {children}
+      </MenuContext.Provider>
+    </ul>
+  )
+}
+
+Menu.defaultProps = {
+  defaultIndex: 0,
+  mode: 'horizontal'
+}
+
+export default Menu
+export {
+  MenuContext
+}
