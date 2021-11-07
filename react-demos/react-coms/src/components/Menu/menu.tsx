120f356ed015ef930213a5fb2d6e726458febd1b
wangwei <wangwei1@mokahr.com>
2021-11-07 23:37:55 +0800

    [react-coms] feat: 新增menu测试和样式

--- react-demos/react-coms/src/components/Menu/menu.tsx
+++ react-demos/react-coms/src/components/Menu/menu.tsx
@@ -35,7 +35,7 @@ const Menu: React.FC<MenuProps> = ({
     onSelect: handleClick
   }
   return (
-    <ul className={classes} style={style}>
+    <ul className={classes} style={style} data-testid="test-menu">
       <MenuContext.Provider value={passedContext}>
         {children}
       </MenuContext.Provider>
