3255fcbe5ee7726039990ce07f13a400635f80da
wangwei <wangwei1@mokahr.com>
2022-01-19 00:13:41 +0800

    [react-coms] feat: 升级扩展 menu 组件写法

--- react-demos/react-coms/src/components/Menu/menu.tsx
+++ react-demos/react-coms/src/components/Menu/menu.tsx
@@ -1,5 +1,6 @@
 import React, { useState, createContext } from 'react'
 import classNames from 'classnames'
+import { MenuItemProps } from './menuItem'
 
 type MenuMode = 'horizontal' | 'vertical'
 type SelectCallback = (selectedIndex: number) => void
@@ -34,10 +35,28 @@ const Menu: React.FC<MenuProps> = ({
     index: currentActive,
     onSelect: handleClick
   }
+  // 确保传入的 children 是符合规则的
+  const renderChildren = () => {
+    // React.Children.map 能够跳过不符合规则的 children 元素
+    return React.Children.map(children, (child, index) => {
+      const childElement = child as React.FunctionComponentElement<MenuItemProps>
+      const { displayName } = childElement.type
+      if (displayName === 'MenuItem') {
+        // return child
+        // 使用组件克隆自动传入 index
+        return React.cloneElement(childElement, {
+          index
+        })
+      } else {
+        console.error('Warnning: Menu has a child which is not a MenuItem component')
+      }
+    })
+  }
+
   return (
     <ul className={classes} style={style} data-testid="test-menu">
       <MenuContext.Provider value={passedContext}>
-        {children}
+        {renderChildren()}
       </MenuContext.Provider>
     </ul>
   )
